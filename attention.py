49c1d3c2916fff89d526e741f0eed7aa20d90252
DingerDave <77742798+DingerDave@users.noreply.github.com>
2022-05-02 21:51:38 -0400

    init attention

---
+++ attention.py
@@ -0,0 +1,22 @@
+import tensorflow as tf 
+import numpy as np
+import math
+import random
+
+class dotProductAttention():
+
+    def __init__(self, d_model, d_k, d_v, h):
+        '''
+        :param d_model: Output dimensionality of the model
+        :param d_k: Dimensionality of queries and keys
+        :param d_v: Dimensionality of values
+        :param h: Number of heads
+        '''
+
+        self.queryDense = tf.keras.layers.Dense(h * d_k)
+        self.keysDense = tf.keras.layers.Dense(h * d_k)
+        self.valuesDense = tf.keras.layers.Dense(h * d_v)
+
+        self.outDense = tf.keras.layers.Dense(d_model)
+
+        
