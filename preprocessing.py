7feacea9e38bd0673866c2c1c1f1bc8a2adebcee
bove1 <90574319+bove1@users.noreply.github.com>
2022-05-09 14:28:34 -0400

    Preprocessing done

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,7 @@ from pycocotools.coco import COCO as pyCOCO
 import numpy as np
 import tensorflow as tf
 import string
+import PIL
 import itertools
 from collections import Counter
 
@@ -12,6 +13,7 @@ class CocoLoader:
         self.dataset = pyCOCO(self.annotations)
         self.id_file = id_file
         self.ids = np.load(self.id_file)
+        np.random.shuffle(self.ids)
         self.curr_batch = 0
         self.create_dict()
 
@@ -24,20 +26,21 @@ class CocoLoader:
         all_words = list(itertools.chain.from_iterable(full_sentences))
         items = list(Counter(all_words).items())
         items.sort(reverse=True, key= lambda x: x[1])
-        print(items)
 
         words_accounted = 0
         self.vocab_dict = {"<STOP>":0, "<UNK>":1}
 
         curr_val = 0
-        while words_accounted < len(all_words) * .9:
-            print(words_accounted)
-            self.vocab_dict[curr_val + 2] = items[curr_val][0]
+        while words_accounted < len(all_words) * .95:
+            self.vocab_dict[items[curr_val][0]] = curr_val + 2
             words_accounted += items[curr_val][1]
             curr_val += 1
 
         self.vocab_size = len(self.vocab_dict)
+        self.dict_vocab = {integer:word for word, integer in self.vocab_dict.items()}
 
+    def get_vocab_dicts(self):
+        return self.vocab_dict, self.dict_vocab
 
     def load(self, batches):
 
@@ -61,17 +64,31 @@ class CocoLoader:
             if len(sentence) > 15:
                 full_sentences[j] = sentence[:15]
 
-            for i, word in enumerate(sentence):
+            for i, word in enumerate(full_sentences[j]):
                 if word not in self.vocab_dict:
-                    sentence[i] = self.vocab_dict["<UNK>"]
+                    full_sentences[j][i] = self.vocab_dict["<UNK>"]
                 else:
-                    sentence[i] = self.vocab_dict[word]
+                    full_sentences[j][i] = self.vocab_dict[word]
         
-        img_ids = [banger["image_id"] for banger in self.dataset.loadAnns(ann_ids)]
-        images = self.dataset.loadImgs(img_ids)
-        print(images)
+        image_ids = [banger["image_id"] for banger in self.dataset.loadAnns(ann_ids)]
+        images = self.dataset.loadImgs(image_ids)
 
-        return tf.one_hot(np.array(full_sentences), self.vocab_size)
+        path_root = "train2014/"
+        images = [path_root + image["file_name"] for image in images]
+        images = [np.asarray(PIL.Image.open(image), dtype=int)
+                for image in images]
+        
+        for i, image in enumerate(images):
+            images[i] = np.pad(
+                image, 
+                ((0, 640 - image.shape[0]), (0, 640 - image.shape[1]), (0, 0)),
+                constant_values=0)
+        
+        images = np.stack(images)
+
+        return tf.one_hot(np.array(full_sentences), self.vocab_size), images
 
 loader = CocoLoader("annotations/captions_train2014.json", "annotations/coco_train_ids.npy")
-print(loader.load(10))
+print(loader.vocab_dict)
+loader.load(256)
+max_h, max_w = 0, 0
