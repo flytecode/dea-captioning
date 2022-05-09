988cefc5da41178b700b96aa55f5abe3b2fb1aa2
DingerDave <77742798+DingerDave@users.noreply.github.com>
2022-05-08 22:11:00 -0400

    actually finished attention

--- decoder.py
+++ decoder.py
@@ -1,2 +1,10 @@
 import tensorflow as transformer
 import numpy as np
+
+
+from attention import dotProductAttention
+from utils import sinusoid_encoding_table, PositionWiseFeedForward
+
+
+class MeshedDecoderLayer(Module):
+    def __init__(self)
